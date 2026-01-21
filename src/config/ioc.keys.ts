1ea439f76274b47eeb21afc5a0a1abcaffa90832
David  Pérez <56732148+DavidPerezz99@users.noreply.github.com>
2026-01-21 04:44:35 -0800

    Create ioc.keys.ts

---
+++ src/config/ioc.keys.ts
@@ -0,0 +1,9 @@
+/**
+ * Declares IoC identifiers as symbols for declaration
+ * and injection throughout the application.
+ */
+export const ContainerKeys = {
+  envConfig: Symbol.for("envConfig"),
+  requestService: Symbol.for("requestService"),
+  apiRequestFactory: Symbol.for("apiRequestFactory")
+};
